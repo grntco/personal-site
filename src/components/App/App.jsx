85d9d955d2433f848d8992f3ac5da19232cb3d66
Grant Collins <grantcollins03@gmail.com>
2024-03-22 16:20:40 -0400

    refactored App to be wrapped in ThemeContext and call Router

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,8 +1,20 @@
 import './App.css'
 import Router from '../Router.jsx'
+import { createContext, useState } from 'react'
+import Page from '../layout/Page/Page.jsx'
+
+const ThemeContext = createContext('light')
 
 function App() {
-    return <Router />
+    const [theme, setTheme] = useState('light')
+
+    return (
+        <ThemeContext.Provider value={theme}>
+            <div className='app'>
+                <Router />
+            </div>
+        </ThemeContext.Provider>
+    )
 }
 
 export default App
--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -1,7 +1,63 @@
-import { createBrowserRouter, RouterProvider} from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Home from './pages/Home/Home.jsx'
 import About from './pages/About/About.jsx'
 import Projects from './pages/Projects/Projects.jsx'
+import App from './App/App.jsx'
+
+const siteNavigationMap = {
+    home: {
+        name: 'Home',
+        path: '/',
+        // element: ,
+        children: [
+            {
+                name: 'About',
+                path: '/about',
+                children: [
+                    {
+                        name: 'Now',
+                        path: '/now',
+                    },
+                    {
+                        name: 'Reading',
+                        path: '/reading',
+                    },
+                ],
+            },
+            {
+                name: 'Projects',
+                path: '/projects',
+                children: [
+                    {
+                        name: 'Personal',
+                        path: '/personal-projects',
+                    },
+                    {
+                        name: 'Professional',
+                        path: '/professional-projects',
+                    },
+                ],
+            },
+            {
+                name: 'Blog',
+                path: '/blog',
+                children: [
+                    {
+                        name: '2023',
+                        path: '/2023',
+                    },
+                    {
+                        name: '2024',
+                        path: '/2024',
+                    },
+                ],
+            },
+        ],
+    },
+    error: {
+        url: '',
+    },
+}
 
 function Router() {
     const router = createBrowserRouter([
