44ac3921cbe89b7dff66a0c60d379e37643de545
Grant Collins <grantcollins03@gmail.com>
2024-09-13 11:37:06 -0400

    add sentient font files; rewrite projects as accordian

--- src/components/layout/ProjectsList/ProjectsList.jsx
+++ src/components/layout/ProjectsList/ProjectsList.jsx
@@ -1,26 +1,49 @@
 import LinkButton from '../../Buttons/LinkButton'
 import styles from './ProjectsList.module.css'
 import projects from './projects.json'
+import { useState } from 'react'
+import IconButton from '../../Buttons/IconButton'
+import ChevronIcon from '../../../assets/icons/chevron-up.svg'
 
 export default function ProjectsList() {
+    const [activeIndex, setActiveIndex] = useState(-1)
+
     return (
-        <ul className={styles.list}>
-            {projects.map((project) => {
+        <ul className={styles.accordian}>
+            {projects.map((project, index) => {
                 return (
-                    <li className={styles.listItem}>
-                        <p className={styles.year}>{project.meta.year}</p>
-                        <div className={styles.info}>
+                    <li className={styles.panel} key={index}>
+                        <div className={styles.panelHeader}>
                             <h3>{project.name}</h3>
-                            <p>{project.exerpt}</p>
+                            <IconButton
+                                src={ChevronIcon}
+                                handleOnClick={() =>
+                                    activeIndex === index
+                                        ? setActiveIndex(-1)
+                                        : setActiveIndex(index)
+                                }
+                            />
                         </div>
-                        <div className={styles.buttonsContainer}>
-                            <LinkButton url={project.links.demo} external>
-                                Demo
-                            </LinkButton>
-                            <LinkButton url={project.links.repo} external>
-                                Repo
-                            </LinkButton>
-                        </div>
+                        {activeIndex === index && (
+                            <div className={styles.panelContent}>
+                                <div>{project.meta.year}</div>
+                                <p>{project.content}</p>
+                                <div className={styles.buttonsContainer}>
+                                    <LinkButton
+                                        url={project.links.demo}
+                                        external
+                                    >
+                                        Demo
+                                    </LinkButton>
+                                    <LinkButton
+                                        url={project.links.repo}
+                                        external
+                                    >
+                                        Repo
+                                    </LinkButton>
+                                </div>
+                            </div>
+                        )}
                     </li>
                 )
             })}
